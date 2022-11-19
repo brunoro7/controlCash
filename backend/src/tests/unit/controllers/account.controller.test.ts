a7acc62b664d6633bb6744e45c58abe12c53e27a
brunoro7 <bruno.ro7c@gmail.com>
2022-11-19 00:32:16 -0300

    test: Criação dos testes para as rotas de .post e .get Account; - relacionados ao services e controllers;

--- backend/src/tests/unit/controllers/account.controller.test.ts
+++ backend/src/tests/unit/controllers/account.controller.test.ts
@@ -67,13 +67,45 @@ describe('Tests for account.controller', () => {
     });
 
     it(`2.2- Testing, if accountsController.createNewAccount ok,
-    return object with new account and status 200`, async () => {
+    return object with new account and status 201`, async () => {
 
       sinon.stub(accountServices, 'createNewAccount').resolves(accountMock);
       const response = await chai.request(api).post('/account').send();
 
       chai.expect(response.body).to.be.deep.equal(accountMock);
       chai.expect(response.status).to.be.equal(201);
+    });
+  });
+
+  describe('3- account.controller/readAccountById', () => {
+
+    it(`3.1- Testing, if accountServices.readAccountById fail,
+    accountController throw error;`, async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const req: any = {
+        params: { id: 1 },
+      };
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const res: any = {
+        status: sinon.stub().callsFake(() => res),
+        json: sinon.stub().returns({}),
+      };
+
+      sinon.stub(accountServices, 'readAccountById').resolves(undefined);
+      const response = await accountController.readAccountById(req, res);
+
+      chai.expect(response).to.be.throw;
+
+    });
+
+    it(`3.2- Testing, if accounts.Controller.readAccountById ok,
+    return object with account and status 200`, async () => {
+
+      sinon.stub(accountServices, 'readAccountById').resolves(accountMock);
+      const response = await chai.request(api).get(`/account/${accountMock.id}`).send();
+
+      chai.expect(response.body).to.be.deep.equal(accountMock);
+      chai.expect(response.status).to.be.equal(200);
     });
   });
 
--- backend/src/tests/unit/services/account.services.test.ts
+++ backend/src/tests/unit/services/account.services.test.ts
@@ -41,4 +41,20 @@ describe('Tests for account.services', () => {
     });
   });
 
+  describe('3- account.services/readAccountById', () => {
+
+    it('3.1- Testing, if Account.findOne fail, accountServices throw error;', () => {
+      sinon.stub(Account, 'findOne').rejects;
+
+      chai.expect(accountServices.readAccountById(0)).to.be.throw;
+    });
+
+    it(`3.2- Testing, if accountServices.readAccountById ok,
+    return object with account;`, async () => {
+      sinon.stub(Account, 'findOne').resolves(accountMock as Account);
+
+      chai.expect(await accountServices.readAccountById(1)).to.be.equal(accountMock);
+    });
+  });
+
 });
