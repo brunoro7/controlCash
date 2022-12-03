3455141eb3cfba36500f63d5bfad65486484fcdf
brunoro7 <bruno.ro7c@gmail.com>
2022-12-03 02:47:40 -0300

    fix: Colocando o consolo.log do backend, como "exceção" para não contar no eslint;

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import api from './api';
 const { BACKEND_PORT } = process.env;
 
 const server = api.listen(
+  // eslint-disable-next-line no-console
   BACKEND_PORT, () => console.log(
     `Server is running on PORT: ${BACKEND_PORT}`,
   )
